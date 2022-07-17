a002b0c79170a4f8377c577263b71edac68b5560
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:50:18 +0200

    Complete transactions service and transactions controller

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,34 +1,67 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { UserAuthGuard } from '../auth/user-auth.guard';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOkResponse, ApiParam,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { FindTransactionsDto } from './dto/find-transactions.dto';
+import { Transaction } from './models/transaction.model';
+import { Transactions } from './models/transactions.model';
+import { E_TRANSACTION_NOT_FOUND, E_UNAUTHORIZED_ACCESS_TO_RESOURCE } from '../common/exceptions';
 
+@ApiBearerAuth()
+@UseGuards(UserAuthGuard)
+@ApiTags('transactions')
+@ApiUnauthorizedResponse()
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
+  @ApiOkResponse({ type: Transaction })
+  @ApiBadRequestResponse()
+  @ApiBody({ type: CreateTransactionDto })
+  create(@Body() dto: CreateTransactionDto, @CurrentUser() currentUser) {
+    return this.transactionsService.create(dto, currentUser);
   }
 
   @Get()
-  findAll() {
-    return this.transactionsService.findAll();
+  @ApiOkResponse({ type: Transactions })
+  findAll(@Query() dto: FindTransactionsDto, @CurrentUser() currentUser) {
+    return this.transactionsService.findAll(dto, currentUser);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionsService.findOne(+id);
+  @ApiOkResponse({ type: Transaction })
+  @ApiNotFoundResponse({ description: E_TRANSACTION_NOT_FOUND })
+  @ApiUnauthorizedResponse({ description: E_UNAUTHORIZED_ACCESS_TO_RESOURCE })
+  findOne(@Param('id') id: number, @CurrentUser() currentUser) {
+    return this.transactionsService.findOne(id, currentUser);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionsService.update(+id, updateTransactionDto);
+  @ApiOkResponse({ type: Transaction })
+  @ApiNotFoundResponse({ description: E_TRANSACTION_NOT_FOUND })
+  @ApiUnauthorizedResponse({ description: E_UNAUTHORIZED_ACCESS_TO_RESOURCE })
+  @ApiBadRequestResponse()
+  update(@Param('id') id: number, @Body() dto: UpdateTransactionDto, @CurrentUser() currentUser) {
+    return this.transactionsService.update(id, dto, currentUser);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionsService.remove(+id);
+  @ApiOkResponse({ type: Transaction })
+  @ApiNotFoundResponse({ description: E_TRANSACTION_NOT_FOUND })
+  @ApiUnauthorizedResponse({ description: E_UNAUTHORIZED_ACCESS_TO_RESOURCE })
+  remove(@Param('id') id: number, @CurrentUser() currentUser) {
+    return this.transactionsService.remove(id, currentUser);
   }
 }
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,26 +1,109 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import {
+  E_TRANSACTION_NOT_FOUND,
+  E_UNAUTHORIZED_ACCESS_TO_RESOURCE,
+  E_USER_NOT_FOUND,
+} from '../common/exceptions';
+import { FindTransactionsDto } from './dto/find-transactions.dto';
+import { ResultsMetadata } from '../common/models/results-metadata.model';
+import { User } from '../users/entities/user.entity';
+import { TransactionTypeEnum } from './enums/transaction-type.enum';
+import { TransactionCategoryEnum } from './enums/transaction-category.enum';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor (
+    @InjectRepository(Transaction)
+    private transactionsRepository: Repository<Transaction>,
+  ) {}
+
+  async create(dto: CreateTransactionDto, currentUser: User) {
+    // Save & return the new transaction
+    let transaction = await this.transactionsRepository.save({
+      ...dto,
+      user: currentUser,
+      type: [TransactionCategoryEnum.revenue, TransactionCategoryEnum.grant, TransactionCategoryEnum.loanPayment, TransactionCategoryEnum.debt].includes(dto.category) ? TransactionTypeEnum.credit : TransactionTypeEnum.debit,
+      reason: dto.reason || null,
+      date: dto.date || new Date()
+    });
+    delete transaction.user;
+    return transaction;
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  async findAll({ offset = 0, limit = 12, sortField, sortOrder }: FindTransactionsDto, currentUser: User) : Promise<{ data: Transaction[], meta: ResultsMetadata }> {
+    // Fetching
+    const [data, count] = await this.transactionsRepository.findAndCount({
+      where: { userId: currentUser.id },
+      order: {
+        [sortField || 'createdAt']: (sortOrder || 'desc').toUpperCase()
+      },
+      skip: offset,
+      take: limit
+    });
+
+    // Returning
+    return {
+      data,
+      meta: {
+        total: count,
+        offset: offset,
+        limit: limit
+      }
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: number, currentUser: User) : Promise<Transaction> {
+    const transaction = await this.findOneByField(id);
+    if (!transaction) throw new NotFoundException(E_TRANSACTION_NOT_FOUND);
+    if (transaction.userId != currentUser.id) throw new UnauthorizedException(E_UNAUTHORIZED_ACCESS_TO_RESOURCE);
+    return transaction;
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async findOneByField(fieldValue: number | string, fieldName: string = 'id'): Promise<Transaction> { // relations => in case e need to load some transaction relations
+    return await this.transactionsRepository.findOne({ where: { [fieldName]: fieldValue } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async getAccountBalance(userId: number): Promise<number> { // relations => in case e need to load some transaction relations
+    const { balance } = await this.transactionsRepository
+      .createQueryBuilder('item')
+      .select("SUM(CASE item.type WHEN 'credit' THEN item.amount ELSE item.amount* -1 END)", "balance")
+      .where({ userId })
+      .getRawOne();
+    return balance;
+  }
+
+  async update(id: number, dto: UpdateTransactionDto, currentUser: User) : Promise<Transaction> {
+    // Check if transaction exists
+    const transaction = await this.findOneByField(id);
+    if (!transaction) throw new NotFoundException(E_TRANSACTION_NOT_FOUND);
+
+    if (transaction.userId != currentUser.id) throw new UnauthorizedException(E_UNAUTHORIZED_ACCESS_TO_RESOURCE);
+
+    // Update and return transaction
+    Object.assign(transaction, {
+      ...dto,
+      type: [TransactionCategoryEnum.revenue, TransactionCategoryEnum.grant, TransactionCategoryEnum.loanPayment, TransactionCategoryEnum.debt].includes(dto.category || transaction.category) ? TransactionTypeEnum.credit : TransactionTypeEnum.debit,
+      reason: dto.reason || transaction.reason,
+      date: dto.date || transaction.date
+    });
+    await this.transactionsRepository.save(transaction);
+    return transaction;
+  }
+
+  async remove(id: number, currentUser: User) : Promise<Transaction> {
+    // Check if transaction exists
+    const transaction = await this.findOneByField(id);
+    if (!transaction) throw new NotFoundException(E_USER_NOT_FOUND);
+
+    if (transaction.userId != currentUser.id) throw new UnauthorizedException(E_UNAUTHORIZED_ACCESS_TO_RESOURCE);
+
+    // Remove and return transaction
+    await this.transactionsRepository.delete(id);
+    return transaction;
   }
 }
