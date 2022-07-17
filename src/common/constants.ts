3229b068d9c6bcad287f819a730cf0ae42ba5d46
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 21:19:01 +0200

    Adds contants.ts and exceptions.ts

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "Accounting App REST API";
+export const APP_DESCRIPTION = "Simple accounting API.";
+export const APP_VERSION = "1.0.0";
+export const PASSWORD_HASH_SALT: number = 10;
+export const JWT_EXPIRES_IN = '7d';
---
+++ src/common/exceptions.ts
@@ -0,0 +1,7 @@
+export const E_USER_EMAIL_TAKEN = "Oops! This email is already taken!";
+export const E_USER_NOT_FOUND = "User not found!";
+export const E_INCORRECT_EMAIL_OR_PASSWORD = "Email or password entered is incorrect!";
+export const E_PASSWORD_INCORRECT = "The password entered is incorrect!";
+export const E_TOO_MANY_REQUESTS = "Too many requests!";
+export const E_TRANSACTION_NOT_FOUND = "User not found! Please make sure the transaction ID entered is valid!";
+export const E_UNAUTHORIZED_ACCESS_TO_RESOURCE = "You are now allowed to access this resource!";
