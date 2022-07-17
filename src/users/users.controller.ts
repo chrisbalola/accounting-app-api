0ad5c58525dfd2c0accfd203731647c1602e5df9
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:25:35 +0200

    Completes users service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor, UseGuards, Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { FindUsersDto } from './dto/find-users.dto';
+import { ApiBasicAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Users } from './models/users.model';
+import { User } from './models/user.model';
+import { AdminAuthGuard } from '../auth/admin-auth.guard';
 
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AdminAuthGuard)
+@ApiBasicAuth()
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @ApiOkResponse({ type: Users })
+  async findAll(@Query() dto: FindUsersDto) : Promise<Users> {
+    return await this.usersService.findAll(dto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @ApiOkResponse({ type: User })
+  findOne(@Param('id') id: number) : Promise<User> {
+    return this.usersService.findOne(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,128 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Raw, Repository } from 'typeorm';
+import { E_PASSWORD_INCORRECT, E_USER_EMAIL_TAKEN, E_USER_NOT_FOUND } from '../common/exceptions';
+import { PASSWORD_HASH_SALT } from '../common/constants';
+import { FindUsersDto } from './dto/find-users.dto';
+import { ResultsMetadata } from '../common/models/results-metadata.model';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor (
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+
+  async updateLastSeenAt(user: User): Promise<User>{
+    user.lastSeenAt=new Date();
+    await this.usersRepository.save(user);
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async verifyPassword (id: number, password: string) {
+    const user = await this.findOneByField(id);
+    if (!user) throw new NotFoundException(E_USER_NOT_FOUND);
+    const bcrypt = require('bcrypt');
+    if (!(await bcrypt.compare(password, user.password))) throw new NotAcceptableException(E_PASSWORD_INCORRECT);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async create(dto: CreateUserDto) {
+    // Check if there is a user with the same email
+    const existingUser = await this.usersRepository.findOne({ where : { email: dto.email } });
+    if (existingUser) {
+      throw new NotAcceptableException(E_USER_EMAIL_TAKEN);
+    }
+
+    // Hashing the password: So that they are protected from whoever can access the database.
+    const bcrypt = require('bcrypt');
+    const hashedPassword = await bcrypt.hash(dto.password, PASSWORD_HASH_SALT);
+
+    // Save & return the new user
+    return this.usersRepository.save({ ...dto, password: hashedPassword });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findAll({ offset = 0, limit = 12, sortField, sortOrder, email, searchQuery }: FindUsersDto = {}) : Promise<{ data: User[], meta: ResultsMetadata }> {
+    // Filtering
+    const where : any = {};
+    if (email) {
+      where.email = email;
+    }
+    if (searchQuery) {
+      where.firstName = Raw(alias => `CONCAT(firstName," ",lastName) LIKE :name`, { name: ('%'+searchQuery+'%') });
+    }
+
+    // Fetching
+    const [data, count] = await this.usersRepository.findAndCount({
+      where,
+      order: {
+        [sortField || 'createdAt']: (sortOrder || 'desc').toUpperCase()
+      },
+      skip: offset,
+      take: limit || null,
+    });
+
+    // Returning
+    return {
+      data,
+      meta: {
+        total: count,
+        offset: offset,
+        limit: limit || null
+      }
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(id: number) : Promise<User> {
+    return await this.usersRepository.findOne({ where: { id } });
+  }
+
+  async findOneByField(fieldValue: number | string, fieldName: string = 'id'): Promise<User> { // relations => in case e need to load some user relations
+    return await this.usersRepository.findOne({ where: { [fieldName]: fieldValue } });
+  }
+
+  async update(id: number, dto: UpdateUserDto) : Promise<User> {
+    // Check if there is a user with the same email
+    if (typeof dto.email != 'undefined') {
+      const existingUser = await this.usersRepository.findOne({ where : { email: dto.email } });
+      if (existingUser) {
+        throw new UnauthorizedException(E_USER_EMAIL_TAKEN);
+      }
+    }
+
+    // Check if user exists
+    const user = await this.findOneByField(id);
+    if (!user) throw new NotFoundException(E_USER_NOT_FOUND);
+
+    // Update and return user
+    Object.assign(user, { ...dto });
+    await this.usersRepository.save(user);
+    return user;
+  }
+
+  async updatePassword(currentPassword: string, newPassword: string, currentUser: User) : Promise<void> {
+    // Checking the current password
+    const bcrypt = require('bcrypt');
+    if (!(await bcrypt.compare(currentPassword, currentUser.password))) throw new NotAcceptableException(E_PASSWORD_INCORRECT);
+
+    // Hashing password
+    const hashedPassword = await bcrypt.hash(newPassword, PASSWORD_HASH_SALT);
+
+    // Update user password
+    Object.assign(currentUser, { password: hashedPassword });
+    await this.usersRepository.save(currentUser);
+  }
+
+  async remove(id: number) : Promise<User> {
+    // Check if user exists
+    const user = await this.findOneByField(id);
+    if (!user) throw new NotFoundException(E_USER_NOT_FOUND);
+
+    // Remove and return user
+    await this.usersRepository.delete(id);
+    return user;
   }
 }
