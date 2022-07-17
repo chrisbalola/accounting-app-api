9448c28ba9920bad599b3bda1430257aa99f85af
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:28:41 +0200

    Add users controllers and services to the module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountController } from './account.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [
+    UsersController,
+    AccountController
+  ],
+  providers: [
+    UsersService,
+  ],
+  exports: [
+    TypeOrmModule,
+    UsersService,
+  ]
 })
 export class UsersModule {}
