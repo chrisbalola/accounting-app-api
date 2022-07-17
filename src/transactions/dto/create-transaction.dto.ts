35ee52d08319eb9747817a576ec14de2db4d239d
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:49:20 +0200

    Add transaction module's DTOs

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTransactionDto {}
+import { IsDate, IsDateString, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { TransactionCategoryEnum } from '../enums/transaction-category.enum';
+
+export class CreateTransactionDto {
+  @IsEnum(TransactionCategoryEnum)
+  @ApiProperty({ enum: Object.values(TransactionCategoryEnum) })
+  readonly category: TransactionCategoryEnum;
+
+  @IsNumber()
+  @ApiProperty()
+  readonly amount: number;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ nullable: true })
+  readonly reason: string;
+
+  @IsDate()
+  @IsOptional()
+  @ApiProperty({ nullable: true, description: "Transaction date and time. Defaults to now (When value is null or undefined)." })
+  readonly date: Date;
+}
---
+++ src/transactions/dto/find-transactions.dto.ts
@@ -0,0 +1,3 @@
+import { FindDto } from '../../common/dto/find.dto';
+
+export class FindTransactionsDto extends FindDto {}
