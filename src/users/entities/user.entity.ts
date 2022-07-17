dd48e21902dd2f863e65d8c3ec8fc5208c4d2fc6
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:20:11 +0200

    Add properties to users.entity.ts file

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: "varchar", length: 50 })
+  firstName: string;
+
+  @Column({ type: "varchar", length: 50 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Exclude() // Using the class-transformer to make sure the password is excluded from results that are returned to the user
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ nullable: true, default: null })
+  lastSeenAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  // --
+
+  // For the properties excluded using class-transformer
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
