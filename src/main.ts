ffc142a28ebe2d2cc8f2f605581c7cb54e32c110
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 21:58:56 +0200

    Set up basic security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
+import 'dotenv/config'
+import { ValidationPipe } from '@nestjs/common';
+import { E_TOO_MANY_REQUESTS } from './common/exceptions';
 
 async function bootstrap() {
+  // -- App Instantiation
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // -- Helmet
+  app.use(helmet());
+
+  // -- Cors setup
+  app.enableCors({
+    origin: false, // Specify the allowed origins.  I'm setting false to allow requests from any origin
+    // Find more configuration options here: https://github.com/expressjs/cors#configuration-options
+  });
+
+  // -- Rate limiting: Limits the number of requests from the same IP in a period of time.
+  // -- More at: https://www.npmjs.com/package/express-rate-limit
+  app.use(rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+    skipSuccessfulRequests: false, // The counting will skip all successful requests and just count the errors. Instead of removing rate-limiting, it's better to set this to true to limit the number of times a request fails. Can help prevent against brute-force attacks
+    message: { "message": E_TOO_MANY_REQUESTS, "statusCode": 403, }
+  }));
+
+  // -- Validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+
+  // -- Start listening
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
