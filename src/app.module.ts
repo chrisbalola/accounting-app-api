671770656cafffe552c2fe7213cb7a382f0238e2
Christian Balola <christianmugisho17@gmail.com>
2022-07-17 22:16:50 +0200

    Generate users module and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -22,6 +24,8 @@ import * as Joi from 'joi';
       }),
     }),
     DatabaseModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
