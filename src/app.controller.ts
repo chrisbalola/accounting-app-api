aa6fe933ef8fba844916ba376544dc4bea4a9d32
Christian Balola <christianmugisho17@gmail.com>
2022-07-18 21:18:21 +0200

    Redirect API entry point to /docs path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiMovedPermanentlyResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Redirect('/docs')
+  @ApiMovedPermanentlyResponse({ description: "Redirects to /docs" })
+  loadDocs() {}
 }
