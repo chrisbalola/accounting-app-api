b925dcf4ba28cc2cbce3f65a513f333fc2c9ddab
Christian Balola <christianmugisho17@gmail.com>
2022-08-03 16:16:09 +0200

    Modify datasource

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -3,12 +3,8 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: ['./**/*.entity.js'], //['./src/**/*.entity.ts'],
+  url: process.env.DB_URL,
+  entities: ['./**/*.entity.js'],
   synchronize: false,
   migrations: ['src/database/migrations/*{.ts,.js}'],
   migrationsRun: true,
