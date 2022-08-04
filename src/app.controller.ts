e0dedb96ea480729f1843bcdbeb6d724c8427095
Christian Balola <christianmugisho17@gmail.com>
2022-08-04 10:36:10 +0200

    Remove home page redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiMovedPermanentlyResponse } from '@nestjs/swagger';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Redirect('/docs')
-  @ApiMovedPermanentlyResponse({ description: "Redirects to /docs" })
-  loadDocs() {}
+  @ApiOkResponse()
+  helloWorld() {
+    return this.appService.getHello();
+  }
 }
